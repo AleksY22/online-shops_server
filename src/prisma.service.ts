e764a47db0149f0e15bc9c073eb0a5ce664a971b
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-02-24 19:38:21 +0300

    add vercel.json

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -7,9 +7,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({
-      // connectionString: process.env.DATABASE_URL as string,
-      connectionString:
-        'postgres://8454ec8413b5ef42a6438291dbe7a26112d7bd56fa7902f88095c2f464d037ae:sk_EwHl5uDes9pwz0t2QzEWt@db.prisma.io:5432/postgres?sslmode=require',
+      connectionString: process.env.DATABASE_URL as string,
     });
     super({ adapter });
   }
